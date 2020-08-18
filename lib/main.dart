04a4e9a92ae0159e8db6fa103fff4edd3cad0ec8
Kazbek Sultanov <kazbek.sultanov@nu.edu.kz>
2020-08-18 18:32:20 +0500

    master4

--- lib/main.dart
+++ lib/main.dart
@@ -33,6 +33,7 @@ class SimpleBlocDelegate extends BlocDelegate {
 //changes in master
 //changes in master2
 //changes in master3
+//changes in master4
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
